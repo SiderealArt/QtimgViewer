fb06a73b1c69e9bff82cdd0563eb75f97baf9387
SiderealArt <nelson22768384@gmail.com>
2022-01-07 09:02:55 +0800

    Update settings.cpp

--- settings.cpp
+++ settings.cpp
@@ -1,4 +1,5 @@
 ﻿#include "settings.h"
+#include <QtWidgets>
 Settings::Settings(QWidget *parent)
 : QWidget(parent)
 {
@@ -23,13 +24,62 @@ Settings::~Settings()
 GeneralTab::GeneralTab(QWidget *parent)
     : QWidget(parent)
 {
+  QGroupBox *permissionsGroup = new QGroupBox(tr("Permissions"));
 
+      QCheckBox *readable = new QCheckBox(tr("Readable"));
+
+
+      QCheckBox *writable = new QCheckBox(tr("Writable"));
+
+
+      QCheckBox *executable = new QCheckBox(tr("Executable"));
+
+
+      QGroupBox *ownerGroup = new QGroupBox(tr("Ownership"));
+
+      QLabel *ownerLabel = new QLabel(tr("Owner"));
+
+      QLabel *groupLabel = new QLabel(tr("Group"));
+
+      QVBoxLayout *permissionsLayout = new QVBoxLayout;
+      permissionsLayout->addWidget(readable);
+      permissionsLayout->addWidget(writable);
+      permissionsLayout->addWidget(executable);
+      permissionsGroup->setLayout(permissionsLayout);
+
+      QVBoxLayout *ownerLayout = new QVBoxLayout;
+      ownerLayout->addWidget(ownerLabel);
+      ownerLayout->addWidget(groupLabel);
+      ownerGroup->setLayout(ownerLayout);
+
+      QVBoxLayout *mainLayout = new QVBoxLayout;
+      mainLayout->addWidget(permissionsGroup);
+      mainLayout->addWidget(ownerGroup);
+      mainLayout->addStretch(1);
+      setLayout(mainLayout);
 }
 
 AppearenceTab::AppearenceTab(QWidget *parent)
   : QWidget(parent)
 {
+  QLabel *fileNameLabel = new QLabel(tr("File Name:"));
 
+      QLabel *pathLabel = new QLabel(tr("Path:"));
+
+      QLabel *sizeLabel = new QLabel(tr("Size:"));
+
+      QLabel *lastReadLabel = new QLabel(tr("Last Read:"));
+
+      QLabel *lastModLabel = new QLabel(tr("Last Modified:"));
+
+      QVBoxLayout *mainLayout = new QVBoxLayout;
+      mainLayout->addWidget(fileNameLabel);
+      mainLayout->addWidget(pathLabel);
+      mainLayout->addWidget(sizeLabel);
+      mainLayout->addWidget(lastReadLabel);
+      mainLayout->addWidget(lastModLabel);
+      mainLayout->addStretch(1);
+      setLayout(mainLayout);
 }
 
 WindowTab::WindowTab(QWidget *parent)
