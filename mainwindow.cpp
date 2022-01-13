812bd7e8ffbf3e175a8f197d8d580f43eb2f6c9a
SiderealArt <nelson22768384@gmail.com>
2022-01-13 22:14:01 +0800

    test github action

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,63 +18,63 @@ switch label to graphicview
 #include <QPainter>
 #include <QtNetwork>
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+  : QMainWindow(parent)
 {
-    setWindowTitle("Daruma");
-    setWindowIcon(QIcon(":/main/resources/icon/app.png"));
-    //center = new QWidget();
-    QWidget *imageside = new QWidget();
-    QHBoxLayout *mainLayout = new QHBoxLayout();
-    mainLayout->setContentsMargins(0,0,0,0);
-    QWidget *treeside = new QWidget();
-    QVBoxLayout *treeLayout = new QVBoxLayout();
-    treeLayout->setContentsMargins(0,0,0,0);
-    QSplitter *splitter = new QSplitter();
-    model = new QFileSystemModel();
-    model->setRootPath("");
-    model->setNameFilters(QStringList() << "*.png" << "*.jpg" << "*.gif" << "*.webp" << "*.jpeg");
-    model->setNameFilterDisables(false);
-    tree = new QTreeView();
-    tree->setModel(model);
-    tree->setUniformRowHeights(true); /*https://stackoverflow.com/questions/29798646/qt5-qtreeview-with-custom-model-and-large-data-very-slow-scrolling*/
-    tree->setHeaderHidden(true);
-    connect(tree, SIGNAL(clicked(QModelIndex)),
-                     this, SLOT(loadfileviatree(QModelIndex)));
-    for (int i = 1; i < model->columnCount(); ++i)
-        tree->hideColumn(i);
-    imgWin = new Label();
-    tempWin = new Label();
-    histogramWin = new QLabel();
-    aWin = new About();
-    sWin = new Settings();
-    thresholdWin = new Threshold();
-    imgWin->resize(500,300);
-    imgWin->setScaledContents(true);
-    QScrollArea *imageScrollArea = new QScrollArea();
-       imageScrollArea->setAlignment(Qt::AlignCenter);
-       imageScrollArea->setFrameShape(QFrame::NoFrame);
-       imageScrollArea->setWidget(imgWin);
-    mainLayout->addWidget(imageScrollArea);
-    imageside->setLayout(mainLayout);
-    treeviewTool = addToolBar("Tree View");
-    treeLayout->addWidget(treeviewTool);
-    treeLayout->addWidget(tree);
-    treeside->setLayout(treeLayout);
-    splitter->addWidget(treeside);
-    splitter->addWidget(imageside);
-    setCentralWidget(splitter);
-    createActions();
-    createMenus();
-    createToolbars();
+  setWindowTitle("Daruma");
+  setWindowIcon(QIcon(":/main/resources/icon/app.png"));
+  //center = new QWidget();
+  QWidget *imageside = new QWidget();
+  QHBoxLayout *mainLayout = new QHBoxLayout();
+  mainLayout->setContentsMargins(0,0,0,0);
+  QWidget *treeside = new QWidget();
+  QVBoxLayout *treeLayout = new QVBoxLayout();
+  treeLayout->setContentsMargins(0,0,0,0);
+  QSplitter *splitter = new QSplitter();
+  model = new QFileSystemModel();
+  model->setRootPath("");
+  model->setNameFilters(QStringList() << "*.png" << "*.jpg" << "*.gif" << "*.webp" << "*.jpeg");
+  model->setNameFilterDisables(false);
+  tree = new QTreeView();
+  tree->setModel(model);
+  tree->setUniformRowHeights(true); /*https://stackoverflow.com/questions/29798646/qt5-qtreeview-with-custom-model-and-large-data-very-slow-scrolling*/
+  tree->setHeaderHidden(true);
+  connect(tree, SIGNAL(clicked(QModelIndex)),
+          this, SLOT(loadfileviatree(QModelIndex)));
+  for (int i = 1; i < model->columnCount(); ++i)
+    tree->hideColumn(i);
+  imgWin = new Label();
+  tempWin = new Label();
+  histogramWin = new QLabel();
+  aWin = new About();
+  sWin = new Settings();
+  thresholdWin = new Threshold();
+  imgWin->resize(500,300);
+  imgWin->setScaledContents(true);
+  QScrollArea *imageScrollArea = new QScrollArea();
+  imageScrollArea->setAlignment(Qt::AlignCenter);
+  imageScrollArea->setFrameShape(QFrame::NoFrame);
+  imageScrollArea->setWidget(imgWin);
+  mainLayout->addWidget(imageScrollArea);
+  imageside->setLayout(mainLayout);
+  treeviewTool = addToolBar("Tree View");
+  treeLayout->addWidget(treeviewTool);
+  treeLayout->addWidget(tree);
+  treeside->setLayout(treeLayout);
+  splitter->addWidget(treeside);
+  splitter->addWidget(imageside);
+  setCentralWidget(splitter);
+  createActions();
+  createMenus();
+  createToolbars();
 }
 
 MainWindow::~MainWindow()
 {
 }
 void MainWindow::createActions(){
-    colorpickerAction = new QAction("Color picker");
-    colorpickerAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/up.png")));
-    connect(colorpickerAction,SIGNAL(triggered()),this,SLOT(colorpicker()));
+  colorpickerAction = new QAction("Color picker");
+  colorpickerAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/up.png")));
+  connect(colorpickerAction,SIGNAL(triggered()),this,SLOT(colorpicker()));
   dirupAction = new QAction("Up to parent directory");
   dirupAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/up.png")));
   connect(dirupAction,SIGNAL(triggered()),this,SLOT(dirup()));
@@ -108,7 +108,7 @@ void MainWindow::createActions(){
   saveAction = new QAction("Save");
   saveAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/save.png")));
   saveAction->setDisabled(true);
-   connect(saveAction,SIGNAL(triggered()),this,SLOT(save()));
+  connect(saveAction,SIGNAL(triggered()),this,SLOT(save()));
   saveAsAction = new QAction("Save as...");
   saveAsAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/saveas.png")));
   saveAsAction->setDisabled(true);
@@ -123,124 +123,123 @@ void MainWindow::createActions(){
   fullscreenAction=new QAction("Fullscreen");
   connect(fullscreenAction,SIGNAL(triggered()),this,SLOT(fullscreen()));
   aboutAction = new QAction("About");
- connect(aboutAction,SIGNAL(triggered()),this,SLOT(aboutMenu()));
+  connect(aboutAction,SIGNAL(triggered()),this,SLOT(aboutMenu()));
   checkupdateAction = new QAction("Check for Updates");
-    penAction = new QAction("Pen");
-    penAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/pen.png")));
-    penAction->setCheckable(true);
-    hFlipAction = new QAction("Horizontal Flip");
-    hFlipAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/hflip.png")));
-    hFlipAction->setDisabled(true);
-    connect(hFlipAction,SIGNAL(triggered()),this,SLOT(hflip()));
-    vFlipAction = new QAction("Vertical Flip");
-    vFlipAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/vflip.png")));
-    vFlipAction->setDisabled(true);
-    connect(vFlipAction,SIGNAL(triggered()),this,SLOT(vflip()));
-    openFileAction = new QAction("&Open File",this);
-    openFileAction->setShortcut(tr("Ctrl+O"));
-    openFileAction->setStatusTip("Open Image File");
-    openFileAction->setIcon(QIcon(":/main/resources/icon/openfile.png"));
-    connect(openFileAction,SIGNAL(triggered()),this,SLOT(showOpenFile()));
-    exitAction = new QAction("&Quit",this);
-    exitAction->setShortcut(tr("Ctrl+Q"));
-    exitAction->setStatusTip("Quit Application");
-    connect(exitAction,SIGNAL(triggered()),this,SLOT(close()));
-    zoomInAction = new QAction("Zoom &In",this);
-    zoomInAction->setShortcut(tr("Ctrl++"));
-    zoomInAction->setIcon(QIcon(":/main/resources/icon/zoomin.png"));
-    zoomInAction->setDisabled(true);
-     connect(zoomInAction,SIGNAL(triggered()),this,SLOT(zoomIn()));
-    zoomOutAction = new QAction("Zoom O&ut",this);
-    zoomOutAction->setShortcut(tr("Ctrl+-"));
-    zoomOutAction->setIcon(QIcon(":/main/resources/icon/zoomout.png"));
-    zoomOutAction->setDisabled(true);
-     connect(zoomOutAction,SIGNAL(triggered()),this,SLOT(zoomOut()));
-     connect(exitAction,SIGNAL(triggered()),imgWin,SLOT(close()));
+  penAction = new QAction("Pen");
+  penAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/pen.png")));
+  penAction->setCheckable(true);
+  hFlipAction = new QAction("Horizontal Flip");
+  hFlipAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/hflip.png")));
+  hFlipAction->setDisabled(true);
+  connect(hFlipAction,SIGNAL(triggered()),this,SLOT(hflip()));
+  vFlipAction = new QAction("Vertical Flip");
+  vFlipAction->setIcon(QIcon(QDir().absoluteFilePath(":/main/resources/icon/vflip.png")));
+  vFlipAction->setDisabled(true);
+  connect(vFlipAction,SIGNAL(triggered()),this,SLOT(vflip()));
+  openFileAction = new QAction("&Open File",this);
+  openFileAction->setShortcut(tr("Ctrl+O"));
+  openFileAction->setStatusTip("Open Image File");
+  openFileAction->setIcon(QIcon(":/main/resources/icon/openfile.png"));
+  connect(openFileAction,SIGNAL(triggered()),this,SLOT(showOpenFile()));
+  exitAction = new QAction("&Quit",this);
+  exitAction->setShortcut(tr("Ctrl+Q"));
+  exitAction->setStatusTip("Quit Application");
+  connect(exitAction,SIGNAL(triggered()),this,SLOT(close()));
+  zoomInAction = new QAction("Zoom &In",this);
+  zoomInAction->setShortcut(tr("Ctrl++"));
+  zoomInAction->setIcon(QIcon(":/main/resources/icon/zoomin.png"));
+  zoomInAction->setDisabled(true);
+  connect(zoomInAction,SIGNAL(triggered()),this,SLOT(zoomIn()));
+  zoomOutAction = new QAction("Zoom O&ut",this);
+  zoomOutAction->setShortcut(tr("Ctrl+-"));
+  zoomOutAction->setIcon(QIcon(":/main/resources/icon/zoomout.png"));
+  zoomOutAction->setDisabled(true);
+  connect(zoomOutAction,SIGNAL(triggered()),this,SLOT(zoomOut()));
+  connect(exitAction,SIGNAL(triggered()),imgWin,SLOT(close()));
 }
 
- void MainWindow::createMenus(){
-    fileMenu = menuBar()->addMenu("&File");
-    fileMenu->addAction(openFileAction);
-    fileMenu->addAction(newWindowAction);
-    fileMenu->addAction(saveAction);
-    fileMenu->addAction(saveAsAction);
-    fileMenu->addAction(printAction);
-    fileMenu->addAction(exitAction);
-    editMenu = menuBar()->addMenu("&Edit");
-    editMenu->addAction(undoAction);
-    editMenu->addAction(redoAction);
-    editMenu->addAction(thresholdAction);
-    viewMenu = menuBar()->addMenu("&View");
-    viewMenu->addAction(histogramAction);
-    viewMenu->addAction(hFlipAction);
-    viewMenu->addAction(vFlipAction);
-    viewMenu->addAction(zoomInAction);
-    viewMenu->addAction(zoomOutAction);
-    viewMenu->addAction(fullscreenAction);
-    viewMenu->addAction(alwaysontopAction);
-    toolsMenu =menuBar()->addMenu("&Tools");
-    shareMenu = toolsMenu->addMenu("Share");
-    toolsMenu->addAction(settingAction);
-    shareMenu->addAction(imgurAction);
-    shareMenu->addAction(imgbbAction);
-    shareMenu->addAction(imageshackAction);
-    helpMenu = menuBar()->addMenu("&Help");
-    helpMenu->addAction(aboutAction);
-    helpMenu->addAction(checkupdateAction);
+void MainWindow::createMenus(){
+  fileMenu = menuBar()->addMenu("&File");
+  fileMenu->addAction(openFileAction);
+  fileMenu->addAction(newWindowAction);
+  fileMenu->addAction(saveAction);
+  fileMenu->addAction(saveAsAction);
+  fileMenu->addAction(printAction);
+  fileMenu->addAction(exitAction);
+  editMenu = menuBar()->addMenu("&Edit");
+  editMenu->addAction(undoAction);
+  editMenu->addAction(redoAction);
+  editMenu->addAction(thresholdAction);
+  viewMenu = menuBar()->addMenu("&View");
+  viewMenu->addAction(histogramAction);
+  viewMenu->addAction(hFlipAction);
+  viewMenu->addAction(vFlipAction);
+  viewMenu->addAction(zoomInAction);
+  viewMenu->addAction(zoomOutAction);
+  viewMenu->addAction(fullscreenAction);
+  viewMenu->addAction(alwaysontopAction);
+  toolsMenu =menuBar()->addMenu("&Tools");
+  shareMenu = toolsMenu->addMenu("Share");
+  toolsMenu->addAction(settingAction);
+  shareMenu->addAction(imgurAction);
+  shareMenu->addAction(imgbbAction);
+  shareMenu->addAction(imageshackAction);
+  helpMenu = menuBar()->addMenu("&Help");
+  helpMenu->addAction(aboutAction);
+  helpMenu->addAction(checkupdateAction);
 }
 void MainWindow::createToolbars(){
-    fileTool = addToolBar("File");
-    fileTool->addAction(zoomInAction);
-    fileTool->addAction(zoomOutAction);
-    fileTool->addAction(clipboardAction);
-    ImageTool = addToolBar("Image");
-    ImageTool->addAction(undoAction);
-    ImageTool->addAction(redoAction);
-    ImageTool->addAction(penAction);
-    ImageTool->addAction(colorpickerAction);
-    ImageTool->addAction(vFlipAction);
-    ImageTool->addAction(hFlipAction);
-    ImageTool->addAction(rotateAction);
+  fileTool = addToolBar("File");
+  fileTool->addAction(zoomInAction);
+  fileTool->addAction(zoomOutAction);
+  fileTool->addAction(clipboardAction);
+  ImageTool = addToolBar("Image");
+  ImageTool->addAction(undoAction);
+  ImageTool->addAction(redoAction);
+  ImageTool->addAction(penAction);
+  ImageTool->addAction(colorpickerAction);
+  ImageTool->addAction(vFlipAction);
+  ImageTool->addAction(hFlipAction);
+  ImageTool->addAction(rotateAction);
 
-    treeviewTool->addAction(openFileAction);
-    treeviewTool->addAction(dirupAction);
-    treeviewTool->addAction(dirhomeAction);
+  treeviewTool->addAction(openFileAction);
+  treeviewTool->addAction(dirupAction);
+  treeviewTool->addAction(dirhomeAction);
 
 }
 void MainWindow::loadFile(QString filename){
 
-    if(filename.contains(".gif",Qt::CaseInsensitive)){
-       qDebug()<< "gif";
-       QMovie *gif = new QMovie(filename);
-       imgWin->setMovie(gif);
-       gif->start();
-}else{
-    qDebug()<< img.load(filename);
-    imgWin->setPixmap(QPixmap::fromImage(img));
-};
-    imgWin->resize(QPixmap::fromImage(img).size());
-    tree->setRootIndex(model->index(filename+"/.."));
-    undoAction->setEnabled(true);
-    redoAction->setEnabled(true);
-    hFlipAction->setEnabled(true);
-    vFlipAction->setEnabled(true);
-    rotateAction->setEnabled(true);
-    printAction->setEnabled(true);
-    saveAction->setEnabled(true);
-    saveAsAction->setEnabled(true);
-    zoomInAction->setEnabled(true);
-    zoomOutAction->setEnabled(true);
+  if(filename.contains(".gif",Qt::CaseInsensitive)){
+      QMovie *gif = new QMovie(filename);
+      imgWin->setMovie(gif);
+      gif->start();
+    }else{
+      qDebug()<< img.load(filename);
+      imgWin->setPixmap(QPixmap::fromImage(img));
+    };
+  imgWin->resize(QPixmap::fromImage(img).size());
+  tree->setRootIndex(model->index(filename+"/.."));
+  undoAction->setEnabled(true);
+  redoAction->setEnabled(true);
+  hFlipAction->setEnabled(true);
+  vFlipAction->setEnabled(true);
+  rotateAction->setEnabled(true);
+  printAction->setEnabled(true);
+  saveAction->setEnabled(true);
+  saveAsAction->setEnabled(true);
+  zoomInAction->setEnabled(true);
+  zoomOutAction->setEnabled(true);
 }
 void MainWindow::showOpenFile(){
-    filename = QFileDialog::getOpenFileName(this, "Open Image",".",tr("Images (*.jpg *.jpeg *.png *.bmp *.gif)"));
-    loadFile(filename);
+  filename = QFileDialog::getOpenFileName(this, "Open Image",".",tr("Images (*.jpg *.jpeg *.png *.bmp *.gif)"));
+  loadFile(filename);
 }
 void MainWindow::zoomIn(){
   imgWin->resize(imgWin->width()*(1.8),imgWin->height()*(1.8));
 }
 
 void MainWindow::zoomOut(){
-imgWin->resize(imgWin->width()*(0.8),imgWin->height()*(0.8));
+  imgWin->resize(imgWin->width()*(0.8),imgWin->height()*(0.8));
 }
 
 void MainWindow::aboutMenu(){
@@ -264,18 +263,18 @@ void MainWindow::saveAs(){
                                           QString(),
                                           tr("Images (*.png)"));
   if (!filename.isEmpty())
-  {
-    imgWin->pixmap().save(filename);
-  }
+    {
+      imgWin->pixmap().save(filename);
+    }
 }
 
 void MainWindow::rotate(){
   QPixmap pixmap(imgWin->pixmap());
-      QTransform tr;
-      tr.rotate(90);
-      pixmap = pixmap.transformed(tr);
-      imgWin->setPixmap(pixmap);
-      imgWin->resize(pixmap.size());
+  QTransform tr;
+  tr.rotate(90);
+  pixmap = pixmap.transformed(tr);
+  imgWin->setPixmap(pixmap);
+  imgWin->resize(pixmap.size());
 }
 void MainWindow::copytoclipboard(){
   clipboard->setPixmap(imgWin->pixmap());
@@ -285,7 +284,7 @@ void MainWindow::imgur(){
   connect(manager, &QNetworkAccessManager::finished,
           this, &MainWindow::replyFinished);
 
- /* QNetworkReply *reply =*/ manager->get(QNetworkRequest(QUrl("http://qt-project.org")));
+  /* QNetworkReply *reply =*/ manager->get(QNetworkRequest(QUrl("http://qt-project.org")));
 }
 
 void MainWindow::replyFinished(){
@@ -308,7 +307,7 @@ void MainWindow::print(){
       painter.setViewport(rect.x(), rect.y(), size.width(), size.height());
       painter.setWindow(pixmap.rect());
       painter.drawPixmap(0, 0, pixmap);
-  }
+    }
 }
 
 void MainWindow::threshold(){
@@ -333,9 +332,9 @@ void MainWindow::histogram(){
   histogramimg.fill(Qt::white);
   QPainter painter;
   for(m=0, i=0;i<256;i++){
-    if(mtx[i]>m){
-      m = mtx[i];
-  }
+      if(mtx[i]>m){
+          m = mtx[i];
+        }
     }
   painter.begin(&histogramimg);
   painter.setPen(Qt::black);
@@ -351,7 +350,7 @@ void MainWindow::histogram(){
 }
 void MainWindow::updateimg(int a){
   int tmp;
-clipboard->setPixmap(tempWin->pixmap());
+  clipboard->setPixmap(tempWin->pixmap());
   result = QImage(QSize(tempWin->pixmap().toImage().width(),tempWin->pixmap().toImage().height()),QImage::Format_Mono);
   for(int j=0;j<tempWin->pixmap().toImage().height();j++){
       for(int i=0;i<tempWin->pixmap().toImage().width();i++){
@@ -368,7 +367,7 @@ void MainWindow::ok(){
 
 void MainWindow::cancel(){
   imgWin->setPixmap(tempWin->pixmap());
-   thresholdWin->close();
+  thresholdWin->close();
 }
 
 void MainWindow::loadfileviatree(QModelIndex index){
@@ -377,11 +376,11 @@ void MainWindow::loadfileviatree(QModelIndex index){
 
 void MainWindow::alwaysontop(){
   if(alwaysontopAction->isChecked()){
-              this->setWindowFlags(this->windowFlags() | Qt::WindowStaysOnTopHint);
+      this->setWindowFlags(this->windowFlags() | Qt::WindowStaysOnTopHint);
       this->show();
-  }
-          else{
-              this->setWindowFlags(this->windowFlags() & ~Qt::WindowStaysOnTopHint);
+    }
+  else{
+      this->setWindowFlags(this->windowFlags() & ~Qt::WindowStaysOnTopHint);
       this->show();
     }
 }
@@ -400,5 +399,5 @@ void MainWindow::dirhome(){
 
 
 void MainWindow::colorpicker(){
-    clipboard->setText("#-0000");
+  clipboard->setText("#-0000");
 }
