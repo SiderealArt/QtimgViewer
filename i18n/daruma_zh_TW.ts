1d6d1e9ad2744dff24870e74a894d2fcaf893b0e
SiderealArt <nelson22768384@gmail.com>
2022-01-21 00:51:46 +0800

    New translations daruma_en.ts (Chinese Traditional)

--- i18n/daruma_zh_TW.ts
+++ i18n/daruma_zh_TW.ts
@@ -49,12 +49,12 @@
     <message>
       <location filename="../adjustment.cpp" line="10"/>
       <source>Warmth</source>
-      <translation type="unfinished">Warmth</translation>
+      <translation>溫暖</translation>
     </message>
     <message>
       <location filename="../adjustment.cpp" line="11"/>
       <source>Hue</source>
-      <translation type="unfinished">Hue</translation>
+      <translation>色調</translation>
     </message>
   </context>
   <context>
@@ -62,7 +62,7 @@
     <message>
       <location filename="../camera.cpp" line="6"/>
       <source>Capture Photo</source>
-      <translation type="unfinished">Capture Photo</translation>
+      <translation>拍攝照片</translation>
     </message>
   </context>
   <context>
@@ -75,39 +75,39 @@
     <message>
       <location filename="../fileinfo.cpp" line="6"/>
       <source>File Name: %1</source>
-      <translation type="unfinished">File Name: %1</translation>
+      <translation>文件名： %1</translation>
     </message>
     <message>
       <location filename="../fileinfo.cpp" line="7"/>
       <source>Path: %1</source>
-      <translation type="unfinished">Path: %1</translation>
+      <translation>路徑： %1</translation>
     </message>
     <message>
       <location filename="../fileinfo.cpp" line="8"/>
       <source>File Size: %1</source>
-      <translation type="unfinished">File Size: %1</translation>
+      <translation>文件大小： %1</translation>
     </message>
     <message>
       <location filename="../fileinfo.cpp" line="9"/>
       <source>Last Read: %1</source>
-      <translation type="unfinished">Last Read: %1</translation>
+      <translation>最後閱讀： %1</translation>
     </message>
     <message>
       <location filename="../fileinfo.cpp" line="10"/>
       <source>Last Modified: %1</source>
-      <translation type="unfinished">Last Modified: %1</translation>
+      <translation>最後修改： %1</translation>
     </message>
     <message>
       <location filename="../fileinfo.cpp" line="11"/>
       <location filename="../fileinfo.cpp" line="12"/>
       <location filename="../fileinfo.cpp" line="13"/>
       <source>Size: %1</source>
-      <translation type="unfinished">Size: %1</translation>
+      <translation>尺寸： %1</translation>
     </message>
     <message>
       <location filename="../fileinfo.cpp" line="14"/>
       <source>Ratio: %1</source>
-      <translation type="unfinished">Ratio: %1</translation>
+      <translation>比率： %1</translation>
     </message>
   </context>
   <context>
@@ -126,7 +126,7 @@
       <location filename="../settings.cpp" line="113"/>
       <location filename="../settings.cpp" line="120"/>
       <source>You must restart the application for these changes to take effect.</source>
-      <translation type="unfinished">You must restart the application for these changes to take effect.</translation>
+      <translation>您必須重新啟動應用程序才能使這些更改生效。</translation>
     </message>
   </context>
   <context>
@@ -139,32 +139,32 @@
     <message>
       <location filename="../mainwindow.cpp" line="81"/>
       <source>Warmth</source>
-      <translation type="unfinished">Warmth</translation>
+      <translation>溫暖</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="84"/>
       <source>Brightness</source>
-      <translation type="unfinished">Brightness</translation>
+      <translation>亮度</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="87"/>
       <source>Contrast</source>
-      <translation type="unfinished">Contrast</translation>
+      <translation>對比</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="90"/>
       <source>Hue</source>
-      <translation type="unfinished">Hue</translation>
+      <translation>色調</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="93"/>
       <source>Saturation</source>
-      <translation type="unfinished">Saturation</translation>
+      <translation>飽和</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="96"/>
       <source>Capture Photo</source>
-      <translation type="unfinished">Capture Photo</translation>
+      <translation>拍攝照片</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="99"/>
@@ -190,7 +190,7 @@
     <message>
       <location filename="../mainwindow.cpp" line="111"/>
       <source>Sepia</source>
-      <translation type="unfinished">Sepia</translation>
+      <translation>棕褐色</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="114"/>
@@ -462,72 +462,72 @@
     <message>
       <location filename="../mainwindow.cpp" line="439"/>
       <source>Adjust Warmth</source>
-      <translation type="unfinished">Adjust Warmth</translation>
+      <translation>調整溫暖</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="448"/>
       <source>Adjust Saturation</source>
-      <translation type="unfinished">Adjust Saturation</translation>
+      <translation>調整飽和度</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="457"/>
       <source>Adjust Brightness</source>
-      <translation type="unfinished">Adjust Brightness</translation>
+      <translation>調整亮度</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="466"/>
       <source>Adjust Hue</source>
-      <translation type="unfinished">Adjust Hue</translation>
+      <translation>調整色調</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="481"/>
       <source>Adjust Contrast</source>
-      <translation type="unfinished">Adjust Contrast</translation>
+      <translation>調整對比度</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="508"/>
       <source>File Name: %1</source>
-      <translation type="unfinished">File Name: %1</translation>
+      <translation>文件名： %1</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="509"/>
       <source>Path: %1</source>
-      <translation type="unfinished">Path: %1</translation>
+      <translation>路徑： %1</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="510"/>
       <source>File Size: %1 Bytes</source>
-      <translation type="unfinished">File Size: %1 Bytes</translation>
+      <translation>文件大小： %1 字節</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="511"/>
       <source>Last Read: %1</source>
-      <translation type="unfinished">Last Read: %1</translation>
+      <translation>最後閱讀： %1</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="512"/>
       <source>Last Modified: %1</source>
-      <translation type="unfinished">Last Modified: %1</translation>
+      <translation>最後修改： %1</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="513"/>
       <source>Width: %1</source>
-      <translation type="unfinished">Width: %1</translation>
+      <translation>寬度： %1</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="514"/>
       <source>Height: %1</source>
-      <translation type="unfinished">Height: %1</translation>
+      <translation>身高： %1</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="515"/>
       <source>Megapixels: %1 MP</source>
-      <translation type="unfinished">Megapixels: %1 MP</translation>
+      <translation>百萬像素： %1 MP</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="517"/>
       <source>Ratio: %1:%2</source>
-      <translation type="unfinished">Ratio: %1:%2</translation>
+      <translation>比例： %1：%2</translation>
     </message>
     <message>
       <location filename="../mainwindow.cpp" line="549"/>
